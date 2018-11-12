2ac64b5ab0230e62fde55f3cad02deb1431dd576
DIR\ramon.silva <ramon.silva@concrete.com.br>
2018-11-12 11:27:28 -0200

    Commit inicial da Aplicação para Apresentação

---
+++ BlazorFromScratch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace BlazorFromScratch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
